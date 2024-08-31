6ac403b686121f4e5c2a998a658c5b7e5ac6bce4
Torng030 <a0983629185@gmail.com>
2024-08-31 22:27:14 +0800

    Create 11063 - B2-Sequence.cpp

---
+++ 11063 - B2-Sequence/11063 - B2-Sequence.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,index=0;
+    while(cin>>n)
+    {
+        int tmp=0,a[n];
+        vector<int> v;
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                tmp=a[i]+a[j];
+                if(a[i]<=a[j])
+                    v.push_back(tmp);
+            }
+        }
+        sort(v.begin(),v.end());
+        bool bl=true;
+        for(int i=0;i<v.size()-1;i++)
+        {
+            for(int j=i+1;j<v.size();j++)
+            {
+                if(v[i]==v[j])
+                {
+                    bl=false;
+                    break;
+                }
+            }
+        }
+        cout<<"Case #"<<++index;
+        if(bl)
+            cout<<": It is a B2-Sequence."<<endl;
+        else
+            cout<<": It is not a B2-Sequence."<<endl;
+        cout<<endl;
+    }
+}
