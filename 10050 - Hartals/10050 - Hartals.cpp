c058383eed7211a8de9a37f43df6898db4c59953
Torng030 <a0983629185@gmail.com>
2024-09-04 23:00:27 +0800

    Create 10050 - Hartals.cpp

---
+++ 10050 - Hartals/10050 - Hartals.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int d,p;
+        cin>>d>>p;
+        int r[p];
+        for(int j=0;j<p;j++)
+            cin>>r[j];
+        //-----cin data-----
+        int w[d+1]={0},ans=0;
+        for(int j=1;j<=d;j++)
+        {
+            for(int k=0;k<p;k++)
+            {
+                if(j%r[k]==0)
+                {
+                    w[j]=1;
+                }
+            }
+            if(w[j]==1&&(j%7!=0&&j%7!=6))
+                ans++;
+        }
+        cout<<ans<<endl;
+    }
+}
