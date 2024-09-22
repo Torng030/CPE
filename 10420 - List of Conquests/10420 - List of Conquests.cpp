5ddd4ba4f3b661dbd99b9b567be13fb82108dae5
Torng030 <a0983629185@gmail.com>
2024-09-22 22:04:43 +0800

    Update 10420 - List of Conquests.cpp

--- 10420 - List of Conquests/10420 - List of Conquests.cpp
+++ 10420 - List of Conquests/10420 - List of Conquests.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 int main()
 {
-    int n;
-    string s1,s2;
-    map<string,int> mp;
-    cin>>n;
-    while(n--)
-    {
-        cin>>s1;
-        mp[s1]++;
-        getline(cin,s2);
-        //cout<<"s2:"<<s2<<endl;
-    }
-    for(auto q:mp)
-        cout<<q.first<<" "<<q.second<<endl;
+	int n;
+	cin>>n;
+	string s1,s2;
+	map<string,int> mp;
+	for(int i=0;i<n;i++)
+	{
+		cin>>s1;
+		mp[s1]++;
+		getline(cin,s2);
+	}
+	for(auto &q:mp)
+		cout<<q.first<<" "<<q.second<<endl;
 }
