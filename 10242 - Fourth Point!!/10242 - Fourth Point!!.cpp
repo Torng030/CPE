d37d10905b138fb6b298c22b5f03d747c2ea206a
Torng030 <a0983629185@gmail.com>
2024-10-10 22:39:36 +0800

    Create 10242 - Fourth Point!!.cpp

---
+++ 10242 - Fourth Point!!/10242 - Fourth Point!!.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	double a[2],b[2],c[2],d[2];
+	while(cin>>a[0]>>a[1]>>b[0]>>b[1]>>c[0]>>c[1]>>d[0]>>d[1])
+	{
+		if(a[0]==c[0]&&a[1]==c[1])
+			cout<<fixed<<setprecision(3)<<b[0]+d[0]-a[0]<<" "<<b[1]+d[1]-a[1];
+		else if(a[0]==d[0]&&a[1]==d[1])
+			cout<<fixed<<setprecision(3)<<b[0]+c[0]-a[0]<<" "<<b[1]+c[1]-a[1];
+		else if(b[0]==c[0]&&b[1]==c[1])
+			cout<<fixed<<setprecision(3)<<a[0]+d[0]-b[0]<<" "<<a[1]+d[1]-b[1];
+		else if(b[0]==d[0]&&b[1]==d[1])
+			cout<<fixed<<setprecision(3)<<a[0]+c[0]-b[0]<<" "<<a[1]+c[1]-b[1];
+		cout<<endl;
+	}
+}
