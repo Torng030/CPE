d886ba7bff4a0f2f78a895d901ec37a38a050b53
Torng030 <a0983629185@gmail.com>
2024-10-11 21:37:29 +0800

    Update 00299 - Train Swapping.cpp

--- 00299 - Train Swapping/00299 - Train Swapping.cpp
+++ 00299 - Train Swapping/00299 - Train Swapping.cpp
@@ -3,29 +3,28 @@ using namespace std;
 int main()
 {
 	int t,n,num;
-	while(cin>>t)
+	cin>>t;
+	while(t--)
 	{
-		while(cin>>n)
+		cin>>n;
+		vector<int> v;
+		for(int i=0;i<n;i++)
 		{
-			vector<int> v;
-			for(int i=0;i<n;i++)
-			{
-				cin>>num;
-				v.push_back(num);
-			}
-			int count=0;
-			for(int i=0;i<n;i++)
-			{
-				for(int j=i+1;j<n;j++)
-				{
-					if(v[i]>v[j])
-					{
-						swap(v[i],v[j]);
-						count++;
-					}
-				}	
-			}
-			cout<<"Optimal train swapping takes "<<count<<" swaps."<<endl;
+			cin>>num;
+			v.push_back(num);
 		}
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n-i-1;j++)
+			{
+				if(v[j]>v[j+1])
+				{
+					swap(v[j],v[j+1]);
+					count++;
+				}
+			}	
+		}
+		cout<<"Optimal train swapping takes "<<count<<" swaps."<<endl;
 	}
 }
